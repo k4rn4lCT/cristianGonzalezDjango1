bb12a069f3b9e7ceb73f228f9a9285f60f22794a
k4rn4lCT <cristian.gonzalez.romero10@gmail.com>
2025-01-11 08:34:22 -0300

    correccion de link principal

--- AppSeriesUY/AppSeriesUY/urls.py
+++ AppSeriesUY/AppSeriesUY/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('SeriesUY/',include('SeriesUY.urls')),
+    path('',include('SeriesUY.urls')),
 ]
